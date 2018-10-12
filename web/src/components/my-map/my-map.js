c80a7b308ca6ea78053ce2dcf98cd57c3a470c3d
Roel van Lanen <roelvanlanen@gmail.com>
2018-10-12 09:34:36 +0200

    Proper markers and lines

--- web/src/components/my-map/my-map.js
+++ web/src/components/my-map/my-map.js
@@ -1,13 +1,22 @@
 import React, { Fragment } from 'react';
-import { Map, TileLayer, CircleMarker } from 'react-leaflet';
+import { Map, TileLayer, CircleMarker, Polyline } from 'react-leaflet';
 import { AHVan } from '../ah-van/ah-van';
 
 const MyMarkersList = ({ markers }) => {
     const items = markers
         .slice(1)
         .map((latLng, index) => (
-            <CircleMarker key={index} center={latLng} radius={30} />
+            <CircleMarker
+                key={index}
+                center={latLng}
+                radius={8}
+                color="#3b9fe2"
+                weight={7}
+                fillColor="#fff"
+                fillOpacity={1}
+            />
         ));
+
     return <Fragment>{items}</Fragment>;
 };
 
@@ -18,6 +27,7 @@ export const MyMap = ({ latLngArr }) => {
                 attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
+            <Polyline positions={latLngArr} color="#3b9fe2" />
             <MyMarkersList markers={latLngArr} />
             <AHVan position={latLngArr[0]} rotation={45} />
         </Map>
